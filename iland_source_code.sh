d6d7a9419131a81e56740aaedd5d9a46456cf9e2
shengkai866 <156604986+shengkai866@users.noreply.github.com>
2025-04-09 01:49:21 -0400

    Update iland_source_code.sh

--- iland_source_code.sh
+++ iland_source_code.sh
@@ -3,11 +3,6 @@
 set -Eeuo pipefail
 set -x
 
-module load gcc/13.2.0
-module load spack
-spack install freeimage
-spack load freeimage
-
 git clone https://github.com/edfm-tum/iland-model.git
 cd iland-model  # Navigate into the repository directory
 git checkout dev # Switch to the "dev" branch
