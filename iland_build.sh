de4b6bc2a5f81de4028763b2a22f2c8ac68e7438
shengkai866 <156604986+shengkai866@users.noreply.github.com>
2025-04-09 01:49:33 -0400

    Update iland_build.sh

--- iland_build.sh
+++ iland_build.sh
@@ -3,6 +3,11 @@
 set -Eeuo pipefail
 set -x
 
+module load gcc/13.2.0
+module load spack
+spack install freeimage
+spack load freeimage
+
 cd iland-model/build/plugins
 qmake ../../src/plugins/plugins.pro
 make
